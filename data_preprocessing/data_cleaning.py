005dbc8dedca1f4e92d119e0849e8ddaed556791
Tony921138 <64877318+Tony921138@users.noreply.github.com>
2020-07-15 12:07:59 +0800

    Update data_cleaning.py

--- data_preprocessing/data_cleaning.py
+++ data_preprocessing/data_cleaning.py
@@ -7,11 +7,12 @@ with open('product_info_NY1(Final).json', 'r', encoding = 'utf-8') as results:
 
 prod_count = 0
 
+# 整理商品名稱
 def clean_name(product):                                      
     product['name'] = BeautifulSoup(product['name'], 'lxml').text
     product['name'] = product['name'].replace('&amp;', '&')
 
-
+# 整理商品價格
 def clean_price(product):
     keyword_list = []
     price_list = []        
@@ -19,30 +20,21 @@ def clean_price(product):
         product['price'] = product['price'].replace('$', '')
         if '-' in product['price']:                               
             product['price'] = (float(product['price'].partition(' - ')[0]) + float(product['price'].partition(' - ')[2])) / 2
-            
-#         elif product['price'] == 'See low price in cart':                            
-#             keyword_list.append(product['keyword'])
-#             for keyword in keyword_list: 
-#                 for product in results_list:
-#                     try:
-#                         if product['keyword'] == keyword:
-#                             price_list.append(float(product['price']))
-#                             product['price'] = sum(price_list) / len(price_list)
-#                     except ValueError:
-#                         continue
-#             price_list = []
+     
+    product['price'] = round(float(product['price']), 2)
 
-#     product['price'] = round(float(product['price']), 2)
-        
+# 整理星星數        
 def clean_star_ratings(product):
     product['star_ratings'] = tuple(product['star_ratings'])
     product['star_ratings'][1] = float(product['star_ratings'][1])
-        
+
+# 整理highlights
 def clean_highlights(product):
     for i in product['highlights']:
         i  = BeautifulSoup(i, 'lxml').text
         i = i.replace('\n', '').replace('●', '').replace('*', '').replace('|', '').replace('see nutrition information for Saturated Fat, and Sodium content', '')
 
+# 整理specifications
 def clean_specifications(product):
     if product['specifications'] != 'NA':
         for k, v in product['specifications'].items():
@@ -51,6 +43,7 @@ def clean_specifications(product):
                 v = v.strip(' ')
                 v = (float(v.partition(' ')[0]), v.partition(' ')[2].lower())
 
+# 主程式進入點
 if __name__ = '__main__':
     for product in results_list:
         clean_name(product)
